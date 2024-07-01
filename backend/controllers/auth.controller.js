853c4942673353260bdc20cc592ab1fab4f2f189
Thejandeera <thejan1010@gmail.com>
2024-07-02 01:13:37 +0530

    routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup = (req,res)=>{
+    res.send("signup user")
+    console.log("signupUser");
+}
+export const login = (req,res)=>{
+    console.log("loginUser");
+}
+export const logout = (req,res)=>{
+    console.log("logoutUser");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.Routes.js"
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
 
     res.send("Hello worldd!");
 });
+
+// app.get("/api/auth/signup",(req,res)=>{
+//     console.log("Signup route")
+// });
+// app.get("/api/auth/login",(req,res)=>{
+//     console.log("login route")
+// });
+// app.get("/api/auth/logout",(req,res)=>{
+//     console.log("logout route")
+// });
+
+app.use("/api/auth",authRoutes)
 
 
 
