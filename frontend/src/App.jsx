75d1bd13775a7fdb1a5f55f9a0605d239ee6ec68
Thejandeera <thejan1010@gmail.com>
2024-07-06 14:41:12 +0530

    Ui design part done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import Login from './pages/login/Login.jsx';
-import SignUp from './pages/signup/SignUp.jsx';
+
 import Home from './pages/home/Home.jsx';
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center' >
-      {/* <SignUp/> */}
+      
       <Home/>
     </div>
   );
