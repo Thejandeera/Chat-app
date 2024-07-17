736d7ba94a17fd80c33ba82def1362a3e84d79f6
Thejandeera <thejan1010@gmail.com>
2024-07-17 23:43:06 +0530

    done until update the database when send a message

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,26 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-	return (
-		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-		</div>
-	);
+    const { loading, conversations } = useGetConversations();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            {conversations.map((conversation, idx) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.length - 1}
+                />
+            ))}
+        </div>
+    );
 };
+
 export default Conversations;
